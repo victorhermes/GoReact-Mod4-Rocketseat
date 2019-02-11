1374ed7cb638c131ced1c7bbbbf6ad5b58bf098c
Victor Hermes <me.victorhermes@gmail.com>
2019-02-11 19:45:17 -0200

    Reactotron

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
 import GlobalStyle from './styles/global';
-
+import './config/reactotron';
 import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
 import Player from './Components/Player';
 
 import { Wrapper, Container, Content } from './styles/components';
-
 import Routes from './routes';
 
 const App = () => (
